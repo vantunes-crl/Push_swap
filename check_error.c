c414dd60a604f13f2538472d83b1248e613621c9
Valerio Antunes de souza junior <vantunes@c1r18s13.42lisboa.com>
2021-05-31 15:52:53 +0100

    norm

--- check_error.c
+++ check_error.c
@@ -9,12 +9,15 @@ int if_order(t_stack *stack)
             return (0);
         i++;
     }
+    free(stack->stackA);
+    free(stack->stackB);
     return (1);
 }
 
 int check_doble(t_stack *stack)
 {
     int *array;
+
     array = malloc(sizeof(int) * stack->size);
     ft_memcpy(array , stack->stackA, sizeof(int) * find_new_size(stack));
     ft_sort(stack->size, array);
@@ -22,7 +25,13 @@ int check_doble(t_stack *stack)
     while (i < stack->size -1)
     {
         if (array[i] == array[i + 1])
+        {
+            free(array);
+            free(stack->stackB);
+            free(stack->stackA);
+            write(1, "Error\n", 6);
             return (1);
+        }
         i++;
     }
     free(array);
--- main.c
+++ main.c
@@ -19,16 +19,10 @@ int main(int argc, char **argv)
             stack.stackA[i] = -1;
         j++;
     }
-    // if (if_order(&stack))
-    // {
-    //     write(1,"ordened\n",7);
-    //     exit(0);
-    // }
-    // if (check_doble(&stack))
-    // {
-    //     write(1,"double\n", 7);
-    //     exit(0);
-    // }
+    if (if_order(&stack))
+        exit(0);
+    if (check_doble(&stack))
+        exit(0);
     if (argc == 4)
         organize_3(&stack);
     else
@@ -46,15 +40,7 @@ int main(int argc, char **argv)
         while (find_new_size_b(&stack) > 0)
             organize_b(&stack);
     }
-    // if (if_order(&stack))
-    // {
-    //    printf("ordened\n");
-    //    exit(0);
-    // }
-    // i = 0;
-    // while (i < stack.size)
-    //    printf("%d ",stack.stackA[i++]);
-    write(1,stack.cmd, ft_strlen(stack.cmd));
+    write(1, stack.cmd, ft_strlen(stack.cmd));
     free(stack.cmd);
     free(stack.stackA);
     free(stack.stackB);
