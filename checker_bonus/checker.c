57d630e1a28022ee4854e935eddddddfa2b2787e
Valerio Antunes de souza junior <vantunes@c1r18s13.42lisboa.com>
2021-05-31 23:27:15 +0100

    bonus

--- checker_bonus/checker.c
+++ checker_bonus/checker.c
@@ -1,6 +1,64 @@
-#include "push_swap.h"
+#include "get_next_line.h"
+#include "../mandatory/push_swap.h"
 #include <stdio.h>
 
-int main()
+void	use_cmds(t_stack *stack, char *line)
 {
+	if (line[0] == 'r' && line[1] == 'r' && line[2] == 'a')
+		rra(stack);
+	if (line[0] == 'r' && line[1] == 'a' && line[2] == '\0')
+		ra(stack);
+	if (line[0] == 'r' && line[1] == 'b' && line[2] == '\0')
+		rb(stack);
+	if (line[0] == 'r' && line[1] == 'r' && line[2] == 'b')
+		rrb(stack);
+	if (line[0] == 's' && line[1] == 'a' && line[2] == '\0')
+		sa(stack);
+	if (line[0] == 's' && line[1] == 'b' && line[2] == '\0')
+		sb(stack);
+	if (line[0] == 'p' && line[1] == 'b' && line[2] == '\0')
+		pb(stack);
+	if (line[0] == 'p' && line[1] == 'a' && line[2] == '\0')
+		pa(stack);
+}
+
+static	void	init(t_stack *stack, char **argv, int argc)
+{
+	int	j;
+	int	i;
+
+	j = 1;
+	stack->cmd = "";
+	stack->size = argc - 1;
+	if (check_double(argv))
+		exit(0);
+	if (check_caracters(argv))
+		exit(0);
+	stack->stackA = ft_calloc(stack->size, sizeof(int));
+	stack->stackB = ft_calloc(stack->size, sizeof(int));
+	i = -1;
+	while (++i < stack->size)
+	{
+		stack->stackA[i] = ft_atoi(argv[j]);
+		if (ft_atoi(argv[j]) == 0)
+			stack->stackA[i] = -1;
+		j++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	char	*line;
+	t_stack	stack;
+
+	init(&stack, argv, argc);
+	while (get_next_line(STDIN_FILENO, &line))
+	{
+		use_cmds(&stack, line);
+	}
+	if (if_order(&stack))
+		write(1, "\033[1;32m[OK]\n", 15);
+	else
+		write(1, "\033[1;31m[KO]\n", 13);
+	free(stack.cmd);
 }
---
+++ checker_bonus/get_next_line.c
@@ -0,0 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vantunes <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/02 11:34:00 by vantunes          #+#    #+#             */
+/*   Updated: 2021/03/02 11:40:21 by vantunes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include "../mandatory/push_swap.h"
+static	char	*rest_lane(char *save)
+{
+	int		i;
+	int		j;
+	char	*ret;
+
+	i = 0;
+	j = 0;
+	if (!save)
+		return (0);
+	while (save[i] && save[i] != '\n')
+		i++;
+	if (!save[i])
+	{
+		free(save);
+		return (0);
+	}
+	ret = (char *)malloc(sizeof(char) * (ft_len(save) - i) + 1);
+	if (!ret)
+		return (0);
+	i++;
+	while (save[i])
+		ret[j++] = save[i++];
+	ret[j] = '\0';
+	free(save);
+	return (ret);
+}
+
+static	char	*take_line(char *str)
+{
+	int		i;
+	char	*new_str;
+
+	i = 0;
+	if (!str)
+		return (0);
+	while (str[i] && str[i] != '\n')
+		i++;
+	new_str = (char *)malloc(sizeof(char) * (i + 1));
+	i = 0;
+	while (str[i] != '\n' && str[i])
+	{
+		new_str[i] = str[i];
+		i++;
+	}
+	new_str[i] = '\0';
+	return (new_str);
+}
+
+int	get_next_line(const int fd, char **line)
+{
+	static char	*save;
+	char		*buff;
+	int			lst_p;
+
+	if (read(fd, 0, 0) || !line || BUFFER_SIZE <= 0)
+		return (-1);
+	buff = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+	while (!check_line(save) && lst_p != 0)
+	{
+		lst_p = read(fd, buff, BUFFER_SIZE);
+		if (lst_p == -1)
+		{
+			free(save);
+			return (-1);
+		}
+		buff[lst_p] = '\0';
+		save = str_join(save, buff);
+	}
+	free(buff);
+	*line = take_line(save);
+	save = rest_lane(save);
+	if (lst_p == 0)
+		return (0);
+	return (1);
+}
